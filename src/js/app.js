62e8d2a5eed51e0930827fab560fa88da7a073e4
AeAProjetos <projetosaa0121@gmail.com>
2026-08-13 14:12:43 -0300

    feat: componentiza menu principal

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import './components/app-header.js';
+
+console.log("App INAV iniciado e componentes carregados.");
---
+++ src/js/components/app-header.js
@@ -0,0 +1,16 @@
+class AppHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <h1>INAV</h1>
+        <nav>
+          <a href="/">Início</a>
+          <a href="/quem-somos">Quem Somos</a>
+          <a href="/educacao-infantil">Educação Infantil</a>
+        </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define('app-header', AppHeader);
